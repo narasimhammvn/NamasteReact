422588fed88fa3aa30442b1f16c14b9b3a0f004a
narasimham <narasimhammvn1@gmail.com>
2024-08-12 14:49:03 +0530

    firstlesson

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading=React.createElement("div",{className:"heading"},
+    React.createElement("div",{},
+        [React.createElement("h1",{},"Hello React"),React.createElement("h2",{},"Hello React")]
+    )
+)
+
+        const root=ReactDOM.createRoot(document.getElementById("root"))
+        root.render(heading)
